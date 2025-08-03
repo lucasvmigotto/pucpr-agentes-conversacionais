f5a04a2b134078040b445048f20cee1bb6278492
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2025-08-03 20:21:50 +0000

    fix(app): create CLI interaction with AI agent

--- src/wpp_agent/app/workflow.py
+++ src/wpp_agent/app/workflow.py
@@ -1,51 +1,36 @@
 from logging import Logger, getLogger
+from sys import exit as exit_app
 
+from ..agent import Agent
 from ..settings import Settings
-from ..utils import ask_reply, choice_accept
-from .agent import Agent
-from .whatsapp_webcrawler import WhatsAppWebCrawler
 
 
-def init_agent_workflow(settings: Settings, logger: Logger | None = None) -> int:
-    _logger: Logger = logger or getLogger(__name__)
-    _agent = Agent(settings=settings.HF)
-
-    with WhatsAppWebCrawler(settings) as crawler:
-        crawler.open_whatsapp().wait(2).scan_qrcode().wait(2).handle_popup_modal().wait(
-            3
-        )
+def init_agent_workflow(
+    agent: Agent, settings: Settings, logger: Logger | None = None
+) -> int:
+    _log: Logger = logger or getLogger(__name__)
+    try:
+        user_input: str = input(settings.UI.initial_prompt)
 
         while True:
-            try:
-                _logger.debug("Refreshing chats...")
+            if user_input.lower().strip() == "":
+                user_input = input("Usuário: ")
+                continue
 
-                if len(chats := crawler.list_chats()) < 1:
-                    _logger.debug("No chats found, waiting...")
-                    crawler.wait(4)
-                    continue
-
-                for chat in chats:
-                    chat.click()
-                    from_who: str = crawler.get_chat_title(chat)
-
-                    if (last_message := crawler.last_message_in()) is None:
-                        _logger.debug(f"No messages received from {from_who}")
-                        continue
-
-                    if not choice_accept(ask_reply(from_who, last_message)):
-                        continue
-
-                    if (
-                        llm_answer := _agent.answer_message(last_message)
-                    ) == settings.HF.LLM_INVALID_TOPIC:
-                        crawler.type_message(
-                            settings.HF.LLM_INVALID_TOPIC_MESSAGE
-                        ).wait(2)
-
-                    crawler.type_message(llm_answer).wait(2)
-                    crawler.send_message()
-
-            except KeyboardInterrupt:
+            if user_input.lower().strip() == settings.UI.EXIT_KEYWORD:
+                print(settings.UI.FINAL_PROMPT)
                 break
 
-        crawler.logout()
+            for message in agent(user_input):
+                print(message)
+
+            user_input: str = input("Usuário: ")
+
+        exit_app(0)
+
+    except KeyboardInterrupt:
+        exit_app(0)
+
+    except Exception as err:
+        _log.exception(f"Error: {err}", exc_info=True)
+        exit_app(1)
